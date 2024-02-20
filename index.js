8d87f6841457f6da978eeb61626b2d48ff970f11
Ahmed Basamad <a.h.basamad.cn@gmail.com>
2024-02-20 11:08:00 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee = {
+    name: "Ahmed",
+    streetAddress: "10010 ny"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, "name": "Sam", "streetAddress": "11 Broadway" };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee["streetAddress"] = "12 Broadway";
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee}
+    delete newEmployee.name
+    return newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name
+    return employee
+}
